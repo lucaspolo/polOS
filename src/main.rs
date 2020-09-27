4fb24fe0412bc7d99515e401a78b589fa1d326b2
Lucas Polo <pradopolo@gmail.com>
2020-09-26 21:55:47 -0300

    VGA Text Mode

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,19 @@
 
 extern crate rlibc;
 
-use core::panic::PanicInfo;
+mod vga_buffer;
 
-static HELLO: &[u8] = b"Hello World!";
+use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-	let vga_buffer = 0xb8000 as *mut u8;
+	println!("Hello World{}", "!");
 
-	for (i, &byte) in HELLO.iter().enumerate() {
-		unsafe {
-			*vga_buffer.offset(i as isize * 2) = byte;
-			*vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-		}
-	}
-    loop {}
+	loop {}
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+	println!("{}", info);
     loop {}
 }

d49911f9578693cf9167c4ab45fc0dc2112164cc
Lucas Polo <pradopolo@gmail.com>
2020-12-16 20:24:39 -0300

    Paging Implementation

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,36 @@
 #![reexport_test_harness_main = "test_main"]
 #![feature(abi_x86_interrupt)]
 
-use core::panic::PanicInfo;
-use pol_os::println;
+use core::{panic::PanicInfo};
+use pol_os::{memory::BootInfoFrameAllocator, println};
+use bootloader::{BootInfo, entry_point};
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+
+entry_point!(kernel_main);
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use pol_os::memory;
+    use x86_64::{structures::paging::Page, VirtAddr};
+
     println!("Hello World{}", "!");
-
     pol_os::init();
 
-    use x86_64::registers::control::Cr3;
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe {
+        BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
 
-    let (level_4_page_table, _) = Cr3::read();
-    println!("Level 4 page table at {:?}", level_4_page_table.start_address());
+    let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
+
+    
     #[cfg(test)]
     test_main();
 
     println!("It did not crash!");
-
     pol_os::hlt_loop();
 }
 

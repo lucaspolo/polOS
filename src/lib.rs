8aa1e38b1980c5dde71d11ca9e5afff8422432b7
Lucas Polo <pradopolo@gmail.com>
2020-12-16 20:24:39 -0300

    Paging Implementation

--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,14 @@ extern crate rlibc;
 
 use core::panic::PanicInfo;
 
+#[cfg(test)]
+use bootloader::{entry_point, BootInfo};
+
 pub mod gdt;
 pub mod interrupts;
 pub mod serial;
 pub mod vga_buffer;
+pub mod memory;
 
 pub trait Testable {
     fn run(&self) -> ();
@@ -60,10 +64,12 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
     }
 }
 
+#[cfg(test)]
+entry_point!(test_kernel_main);
+
 /// Entry point for `cargo test`
 #[cfg(test)]
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+fn test_kernel_main(_boot_info: &'static BootInfo) -> ! {
     init();
     test_main();
     hlt_loop();

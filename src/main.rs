f888dcb794f8e6077ae0b84d6901e39eea7573e9
Lucas Polo <pradopolo@gmail.com>
2021-03-03 22:26:28 -0300

    Heap Allocation

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,16 @@ use core::{panic::PanicInfo};
 use pol_os::{memory::BootInfoFrameAllocator, println};
 use bootloader::{BootInfo, entry_point};
 
+extern crate alloc;
+
+use alloc::{boxed::Box, vec, vec::Vec, rc::Rc};
+
 
 entry_point!(kernel_main);
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use pol_os::allocator;
     use pol_os::memory;
-    use x86_64::{structures::paging::Page, VirtAddr};
+    use x86_64::{VirtAddr};
 
     println!("Hello World{}", "!");
     pol_os::init();
@@ -24,11 +29,24 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
         BootInfoFrameAllocator::init(&boot_info.memory_map)
     };
 
-    let page = Page::containing_address(VirtAddr::new(0));
-    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+    allocator::init_heap(&mut mapper, &mut frame_allocator)
+        .expect("heap initialization failed");
 
-    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
-    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
+    let heap_value = Box::new(41);
+    println!("heap_value at {:p}", heap_value);
+
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+
+    println!("vec at {:p}", vec.as_slice());
+
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!("current referente count is {}", Rc::strong_count(&cloned_reference));
+    core::mem::drop(reference_counted);
+    println!("reference count is {} now", Rc::strong_count(&cloned_reference));
 
     
     #[cfg(test)]
